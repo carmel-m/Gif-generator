45cad7e156cad7c59e24c956a1ea25f2fa656d72
Carmel Meyer <cemeyer@elkera.com>
2019-10-27 14:12:47 -0400

    make all buttons work, gif stills display

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,22 +2,22 @@
 // In this assignment, you'll use the GIPHY API to make a dynamic web page that populates with gifs of your choice. To finish this task, you must call the GIPHY API and use JavaScript and jQuery to change the HTML of your site.
 
 // create an array of strings related to one topic
-    // save to a variable (animalsArray)
+// save to a variable (animalsArray)
 
 // create buttons of the items/strings in the array in HTML 
-    // use a loop that appends a button for each string in the array
+// use a loop that appends a button for each string in the array
 
 // when user clicks one of the buttons, page will get and load 10 static gif images from Giphy API
-    // when image is clicked, gif should animate
-    // when clicked again, go back to still
+// when image is clicked, gif should animate
+// when clicked again, go back to still
 
 // display rating under each gif
 
 // add form to page that takes a value from user input box and adds it to animalsArray (push)
-    // make function to call each topic in array and remake the buttons on the page (to include user's buttons)
+// make function to call each topic in array and remake the buttons on the page (to include user's buttons)
 
 // important GIPHY parameters
-    // 'q' (search query term), 'limit' (max number of records to return), 'rating' (filter results by rating)
+// 'q' (search query term), 'limit' (max number of records to return), 'rating' (filter results by rating)
 
 
 // GIPHY API key = qAE3KGzdAFsmPr0NI3dcRyj3Cuzim6Xs
@@ -44,6 +44,26 @@ function displayAnimals() {
     }).then(function (response) {
         console.log(queryURL);
         console.log(response);
+
+        for (var i = 0; i < animalsArray.length; i++) {
+
+
+            // create new div to display gifs/ratings
+            var gifDiv = $("<div>");
+
+            // get and display rating
+            var rating = response.data[i].rating;
+            var ratingDisplay = $("<p>").text("Rating: " + rating);
+            gifDiv.append(ratingDisplay);
+
+            // get and display gif image
+            var image = response.data[i].images.fixed_height_still.url;
+            var imageDisplay = $("<img src='" + image + "'/>");
+            gifDiv.append(imageDisplay);
+
+            // display gifDiv above previous 
+            $("#gif-display").prepend(gifDiv);
+        }
     })
 
 }
@@ -62,12 +82,13 @@ function renderButtons() {
     }
 }
 
-// function for 'go/add animal' button
+// function for 'go' (add animal) button
 $("#add-animal").on("click", function (event) {
     event.preventDefault();
     var newAnimal = $("#animal-input").val().trim();
     animalsArray.push(newAnimal);
 
+    renderButtons();
 })
 
 // click event for animals
