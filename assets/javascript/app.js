03c09d79eaf637a22f723cb812e7272abf7cea01
Carmel Meyer <cemeyer@elkera.com>
2019-10-27 13:37:47 -0400

    add ajax call and click events

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,39 +2,29 @@
 // In this assignment, you'll use the GIPHY API to make a dynamic web page that populates with gifs of your choice. To finish this task, you must call the GIPHY API and use JavaScript and jQuery to change the HTML of your site.
 
 // create an array of strings related to one topic
-// save to a variable called 'topics'
+    // save to a variable (animalsArray)
 
 // create buttons of the items/strings in the array in HTML 
-// use a loop that appends a button for each string in the array
+    // use a loop that appends a button for each string in the array
 
 // when user clicks one of the buttons, page will get and load 10 static gif images from Giphy API
-// when image is clicked, gif should animate
-// when clicked again, go back to still
+    // when image is clicked, gif should animate
+    // when clicked again, go back to still
 
 // display rating under each gif
 
-// add form to page that takes a value from user input box and adds it to the 'topics' array (push)
-// make function to call each topic in array and remake the buttons on the page (to include user's buttons)
+// add form to page that takes a value from user input box and adds it to animalsArray (push)
+    // make function to call each topic in array and remake the buttons on the page (to include user's buttons)
 
 // important GIPHY parameters
-// 'q' (search query term), 'limit' (max number of records to return), 'rating' (filter results by rating)
+    // 'q' (search query term), 'limit' (max number of records to return), 'rating' (filter results by rating)
 
 
 // GIPHY API key = qAE3KGzdAFsmPr0NI3dcRyj3Cuzim6Xs
 
 
 
-
-
-//javascript, jQuery
-// var xhr = $.get("https://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=YOUR_API_KEY&limit=5");
-// xhr.done(function(data) { console.log("success got data", data); });
-
-
-
-
 // VARIABLES ============================================================
-
 
 var animalsArray = ["kangaroo", "wombat", "cockatoo", "emu", "snake", "koala", "shark", "echidna", "platypus", "spider", "quokka", "quoll", "dingo", "wallaby", "crocodile"]
 
@@ -45,9 +35,18 @@ var animalsArray = ["kangaroo", "wombat", "cockatoo", "emu", "snake", "koala", "
 
 function displayAnimals() {
 
+    var animal = $(this).attr("data-name");
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + animal + "&api_key=qAE3KGzdAFsmPr0NI3dcRyj3Cuzim6Xs&limit=10";
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
+        console.log(queryURL);
+        console.log(response);
+    })
 
 }
-
 
 // display buttons
 function renderButtons() {
@@ -66,11 +65,13 @@ function renderButtons() {
 // function for 'go/add animal' button
 $("#add-animal").on("click", function (event) {
     event.preventDefault();
-
     var newAnimal = $("#animal-input").val().trim();
-
     animalsArray.push(newAnimal);
 
 })
 
+// click event for animals
+$(document).on("click", ".animal", displayAnimals);
+
+// displays buttons
 renderButtons();
